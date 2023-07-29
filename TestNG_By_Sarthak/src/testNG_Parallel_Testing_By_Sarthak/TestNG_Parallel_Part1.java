655d7bcaa1250138d68a395d52fa475c715a6c73
vivek <vivek.sirsath1@gmail.com>
2023-07-29 14:28:17 +0530

    TestNG Parallel_Testing using methods and using classes is added

---
+++ TestNG_By_Sarthak/src/testNG_Parallel_Testing_By_Sarthak/TestNG_Parallel_Part1.java
@@ -0,0 +1,51 @@
+package testNG_Parallel_Testing_By_Sarthak;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNG_Parallel_Part1 {
+	WebDriver driver;
+
+   /* If we wish to execute one method 'tc1' in multiple threads
+	* Thread means instances
+	*/
+	@Test(threadPoolSize = 3, invocationCount = 3)
+	public void tc1() {
+		System.out.println("This is TC1 logic: " + Thread.currentThread().getId());
+		WebDriverManager.firefoxdriver().setup();
+		driver= new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+	}
+
+	@Test
+	public void tc2() {
+		System.out.println("This is TC2 logic: " + Thread.currentThread().getId());
+		// We can open this with ChromeDriver also
+		WebDriverManager.firefoxdriver().setup();
+		driver= new FirefoxDriver();     // We can open this with ChromeDriver also
+		driver.get("https://testautomationpractice.blogspot.com/");
+	}
+	
+	/*
+	 * Situation - methods are 2, thread-count = 2, if we run xml file It will
+	 * allocate different id for 2 methods like below in console
+	 * This is TC2 logic: 17 
+	 * This is TC1 logic: 16
+	 */
+	
+	@Test
+	public void tc3() {
+		System.out.println("This is TC3 logic: " + Thread.currentThread().getId());
+	}
+	
+	/*
+	 * Situation - methods are 3, thread-count = 2, if we run xml file It will
+	 * allocate same id for 2 methods like below in console
+	 * This is TC2 logic: 17 
+	 * This is TC1 logic: 16
+	 * This is TC3 logic: 17
+	 */
+}
---
+++ TestNG_By_Sarthak/src/testNG_Parallel_Testing_By_Sarthak/TestNG_Parallel_Part2.java
@@ -0,0 +1,40 @@
+package testNG_Parallel_Testing_By_Sarthak;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNG_Parallel_Part2 {
+	WebDriver driver;
+
+   /* If we wish to execute one method 'tc1' in multiple threads
+	* Thread means instances
+	*/
+	@Test
+	public void tc4() {
+		System.out.println("This is TC4 logic: " + Thread.currentThread().getId());
+		WebDriverManager.firefoxdriver().setup();
+		driver= new FirefoxDriver();
+		driver.get("https://www.ilovepdf.com/pdf_to_word");
+	}
+
+	@Test
+	public void tc5() {
+		System.out.println("This is TC5 logic: " + Thread.currentThread().getId());
+		// We can open this with ChromeDriver also
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();     // We can open this with ChromeDriver also
+		driver.get("https://www.iloveimg.com/convert-to-jpg/webp-to-jpg");
+	}
+	
+	/*
+	 * Situation - methods are 2, thread-count = 2, if we run xml file It will
+	 * allocate different id for 2 methods like below in console
+	 * This is TC2 logic: 17 
+	 * This is TC1 logic: 16
+	 */
+	
+}
